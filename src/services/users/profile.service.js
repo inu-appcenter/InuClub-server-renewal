224e6b8c4b7f52859147e1376d1f032c7bd4e64f
bbaktaeho <lth9036@gmail.com>
2020-10-07 02:28:48 +0900

    Update: User Profile Feature

--- src/services/users/profile.service.js
+++ src/services/users/profile.service.js
@@ -1,5 +1,28 @@
+const { inuAuth } = require('../../utils/axios.util');
+
 const ProfileService = {
-  modifyAccount: () => {},
+  /**
+   * @returns Promise<수정 응답이 담긴 객체>
+   */
+  modifyAccount: async ({
+    studentId,
+    password,
+    newPassword,
+    phone,
+    major,
+    name,
+  }) => {
+    const requestBody = {
+      id: studentId,
+      passwd: password,
+      newPasswd: newPassword,
+      tel: phone,
+      major,
+      name,
+    };
+    const { data } = await inuAuth.post('/changeInfo', requestBody);
+    return data;
+  },
 };
 
 module.exports = ProfileService;
